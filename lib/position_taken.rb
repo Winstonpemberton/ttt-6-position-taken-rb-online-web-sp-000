a0c745cdad0b2d3530a850b0825e97013330ff62
Winston Pemberton <winstonpemberton@gmail.com>
2019-10-24 22:02:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,11 @@
 # code your #position_taken? method here!
 
   def position_taken?(board, index,default_choice = "X")
-
+    if board[index] == " "
+      false
+    elsif board[index] == ""
+      false
+    else
+      true
 
   end
